c9d1cd10ca2b9fc9819ca0634ea16a303002a8e5
GhoziTech <hafid.ghozii@gmail.com>
2024-03-06 08:10:02 +0700

    ganti scaffold daftar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_02_27_035016) do
-  create_table "posts", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "title"
     t.text "content"
