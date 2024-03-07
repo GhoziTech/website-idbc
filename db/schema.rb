742d707f3ad5187e6abef2a7b6354af6ff32426c
GhoziTech <hafid.ghozii@gmail.com>
2024-03-07 22:15:26 +0700

    Style Layout Sebagian

--- db/schema.rb
+++ db/schema.rb
@@ -61,31 +61,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_034559) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "pendaftarans", force: :cascade do |t|
-    t.string "sekolah"
-    t.string "nama"
-    t.string "logo"
-    t.string "gelombang"
-    t.date "tanggalAwal_pendaftaran"
-    t.date "tanggalAkhir_pendaftaran"
-    t.string "dibuka"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "name"
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
